c00be1ebe8441bfab77324d076ab7dcf2b5e72dd
tim <me@tgibbs.uk>
2025-12-15 22:15:38 +0000

    2015 pt1

---
+++ 2015/day01/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+await Part2();
+return;
+
+async Task Part1()
+{
+    var line = await File.ReadAllTextAsync("input.txt");
+
+    var floor = 0;
+    for (var i = 0; i < line.Length; i++)
+    {
+        switch (line[i])
+        {
+            case '(': floor++; break;
+            case ')': floor--; break;
+        }
+    }
+
+    Console.WriteLine(floor);
+}
+
+async Task Part2()
+{
+    var line = await File.ReadAllTextAsync("input.txt");
+
+    var floor = 0;
+    for (var i = 0; i < line.Length; i++)
+    {
+        switch (line[i])
+        {
+            case '(': floor++; break;
+            case ')': floor--; break;
+        }
+
+        if (floor < 0)
+        {
+            Console.WriteLine(i+1);
+            break;
+        }
+    }
+}
