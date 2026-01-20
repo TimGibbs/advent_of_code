4a993dd5e2f0d4979b86ad238b92ba9150259af5
tim <me@tgibbs.uk>
2026-01-20 09:12:38 +0000

    2017 day 10

---
+++ 2017/day10-Knot Hash/Program.cs
@@ -0,0 +1,123 @@
+﻿// @algorithm Knot Hash
+// @category Hashing / Circular List Manipulation
+// @input
+//   Part1: Comma-separated integers
+//   Part2: Raw string (ASCII codes + fixed suffix)
+// @problem
+//   Part1: Perform one round of knotting and output product of first two values
+//   Part2: Compute full Knot Hash and output hex digest
+// @parts
+//   Part1: Single-round circular reversal
+//   Part2: 64-round knotting + dense hash via XOR
+// @model
+//   - Circular array with current position and skip size
+//   - Segment reversal with wraparound
+// @technique
+//   - Span-based in-place reversal
+//   - Temporary buffer for wrapped segments
+//   - Chunked XOR reduction (dense hash)
+// @data-structures
+//   - Fixed-size int array (256 elements)
+//   - Stack-allocated buffer for reversals
+// @complexity
+//   Time: O(n * rounds)
+//   Space: O(n)
+// @notes
+//   - Lengths larger than list size are ignored
+//   - Dense hash is formed by XOR-ing blocks of 16 values
+
+await Part1();
+await Part2();
+return;
+
+async Task Part1()
+{
+    var line = await File.ReadAllTextAsync("input.txt");
+    var lengths = line.Split(',').Select(int.Parse).ToArray();
+    var loop = Enumerable.Range(0, 256).ToArray();
+    var skipSize = 0;
+    var position = 0;
+    
+    Span<int> buffer = stackalloc int[loop.Length];
+
+    foreach (var length in lengths)
+    {
+        if (length > loop.Length)
+            continue;
+
+        if (position + length <= loop.Length)
+        {
+            loop.AsSpan(position, length).Reverse();
+        }
+        else
+        {
+            var firstPart = loop.Length - position;
+            var secondPart = length - firstPart;
+
+            var temp = buffer[..length];
+
+            loop.AsSpan(position, firstPart).CopyTo(temp);
+            loop.AsSpan(0, secondPart).CopyTo(temp[firstPart..]);
+
+            temp.Reverse();
+
+            temp[..firstPart].CopyTo(loop.AsSpan(position, firstPart));
+            temp[firstPart..].CopyTo(loop.AsSpan(0, secondPart));
+        }
+
+        position = (position + length + skipSize) % loop.Length;
+        skipSize++;
+    }
+
+    Console.WriteLine(loop[0] * loop[1]);
+}
+
+async Task Part2()
+{
+    var line = await File.ReadAllTextAsync("input.txt");
+    var lengths = line.Trim().Select(x => (int)x).Concat([17, 31, 73, 47, 23]).ToArray();
+    var loop = Enumerable.Range(0, 256).ToArray();
+    var skipSize = 0;
+    var position = 0;
+
+    Span<int> buffer = stackalloc int[loop.Length];
+    for (var i = 0; i < 64; i++)
+    {
+        foreach (var length in lengths)
+        {
+            if (length > loop.Length)
+                continue;
+
+            if (position + length <= loop.Length)
+            {
+                loop.AsSpan(position, length).Reverse();
+            }
+            else
+            {
+                var firstPart = loop.Length - position;
+                var secondPart = length - firstPart;
+
+                var temp = buffer[..length];
+
+                loop.AsSpan(position, firstPart).CopyTo(temp);
+                loop.AsSpan(0, secondPart).CopyTo(temp[firstPart..]);
+
+                temp.Reverse();
+
+                temp[..firstPart].CopyTo(loop.AsSpan(position, firstPart));
+                temp[firstPart..].CopyTo(loop.AsSpan(0, secondPart));
+            }
+
+            position = (position + length + skipSize) % loop.Length;
+            skipSize++;
+        }
+    }
+
+    var grouped = loop
+        .Index()
+        .GroupBy(x => x.Index / 16, v => v.Item)
+        .Select(x => x.Aggregate((a, b) => a ^ b))
+        .Select(x => x.ToString("X2").ToLower())
+        .ToArray();
+    Console.WriteLine(string.Join("",grouped));
+}
